5a1f776d67d0c5f66b3f5b32d6a02d10017fa7f5
Liana_Kolodzheieva@example,com <liana.kolodgeeva@gmail.com>
2024-03-06 08:59:31 +0100

    add homework10

---
+++ homework10/JS.js
@@ -0,0 +1,64 @@
+
+// _______1______
+// x = 8;
+// y = 3;
+// alert(x > y);
+
+// _______2______
+// x = 16;
+// y = 16;
+// alert(x == y);
+
+// _______3______
+// x = 20;
+// y = 12;
+// alert(x !== y);
+
+
+// _______4______
+// let age = 6;
+// let voteable = (age < 18) ? "Занадто молодий" : "Вам 18 або більше";
+// alert(voteable);
+
+// _______5______
+// let x = 6;
+// let y = 5;
+// if (x > y){
+//   alert("Hello World");
+// }
+
+// _______6______
+// let x = 5;
+// let y = 6;
+// if(x > y){
+//     alert("Hello World");
+// } else{
+//     alert("Goodbye");
+// }
+
+// _______7______
+// let fruits = "Apple";
+// switch(fruits) {
+//    case "Banana":
+//     alert("Hello");
+//     break;
+//   case "Apple":
+//     alert("Welcome");
+//     break;    
+// }
+
+// _______8______
+// 8.Додайте розділ, який сповіщатиме ("Ні"), якщо фрукти не є ні "банан", ні "яблуко".
+// let fruits = "fdfg";
+// switch(fruits) {
+//   case "Banana":
+//     alert("Hello")
+//     break;
+//   case "Apple":
+//     alert("Welcome")
+//     break;
+//   default:
+//     alert("Neither");
+// }
+
+
