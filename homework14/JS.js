7c5a5b05958eda19b76faf92e657e41bc42d10c4
liana kolodgeeva <liana.kolodgeeva@gmail.com>
2024-04-12 22:51:36 +0200

    homework14

--- homework14/JS.js
+++ homework14/JS.js
@@ -2,22 +2,29 @@
 
 document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault(); 
-
+// збереження інформації
     var name = document.getElementById("name").value;
     var date = document.getElementById("date").value;
     var email = document.getElementById("email").value;
     var number = document.getElementById("number").value;
     var checkbox = document.getElementById("checkbox").checked;
-
+// перевірка
     if (name === "" || date === "" || email === "" || number === "" || !checkbox) {
         alert("Please, fill in all fields of the form and confirm your mind.");
         return;
     }
 
-    var userInfo = "Name: " + name + "<br>" +
-                   "Date: " + date + "<br>" +
-                   "Email: " + email + "<br>" +
-                   "Phone: " + number;
+    var userInfo = "<p>Name: " + name + "</p>" +
+                   "<p>Date: " + date + "</p>" +
+                   "<p>Email: " + email + "</p>" +
+        "<p>Phone: " + number + "</p>" +
+        "<p>_________________________</p>"
 
-    document.getElementById("output").innerHTML = userInfo;
+    // переніс інформаціі
+    document.getElementById("output").insertAdjacentHTML('beforeend', userInfo);
+
+    // чистка форми
+    document.getElementById("form").reset();
 });
+
+
